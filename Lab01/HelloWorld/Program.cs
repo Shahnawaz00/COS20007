f91a1ff4c9b97b3fa7fe52abad946f97dc69d9ea
Shahnawaz00 <89706280+Shahnawaz00@users.noreply.github.com>
2023-03-07 18:12:06 +1100

    first commit

---
+++ Lab01/HelloWorld/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Message myMessage;
+            Message[] messages = new Message[5];
+            string name;
+
+            myMessage = new Message ("Hello World - from message object");
+            myMessage.Print();
+
+            messages[0] = new Message("Hello");
+            messages[1] = new Message("Welcome");
+            messages[2] = new Message("Hi");
+            messages[3] = new Message("OK");
+            messages[4] = new Message("Bye");
+
+            Console.WriteLine("Enter name: ");
+            name = Console.ReadLine();
+
+            if (name.ToLower() == "leah")
+            {
+                messages[0].Print();
+            }
+            else if (name.ToLower() == "abi")
+            {
+                messages[1].Print();
+            } 
+            else if (name.ToLower() == "yu")
+            {
+                messages[2].Print();
+            }
+            else if (name.ToLower() == "raza")
+            {
+                messages[3].Print();
+            }
+            else if (name.ToLower() == "alba")
+            {
+                messages[4].Print();
+            }
+            else
+            {
+                Console.WriteLine("unknown name");
+            }
+
+        }
+    }
+}
